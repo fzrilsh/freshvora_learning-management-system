2ba25eae6f0c2485291f3dea109cdf1621e9a3bf
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2025-09-14 13:53:13 +0700

    update DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// ...existing code...
 import {
   Box,
   Grid,
@@ -12,6 +12,8 @@ import ProgressChart from '../components/widgets/ProgressChart';
 import OngoingClass from '../components/widgets/OngoingClass';
 import PendingAssessments from '../components/widgets/PendingAssessments';
 import InformationSlider from '../components/widgets/InformationSlider';
+import WidgetSkeleton from '../components/widgets/WidgetSkeleton';
+import React, { useState, useEffect } from 'react';
 import Footer from '../components/Footer';
 
 const DashboardContainer = styled(Box)(({ theme }) => ({
@@ -38,6 +40,12 @@ const WelcomeSubtitle = styled(Typography)(({ theme }) => ({
 }));
 
 const DashboardPage = () => {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 1200);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <DashboardContainer>
       <Container maxWidth="xl">
@@ -51,12 +59,12 @@ const DashboardPage = () => {
         <Box sx={{ display: 'flex', flexDirection: { xs: 'column', lg: 'row' }, gap: 3 }}>
           {/* Progress Chart - 60% width on desktop */}
           <Box sx={{ width: { xs: '100%', lg: '60%' } }}>
-            <ProgressChart />
+            {loading ? <WidgetSkeleton height={260} /> : <ProgressChart />}
           </Box>
 
           {/* Ongoing Class - 40% width on desktop */}
           <Box sx={{ width: { xs: '100%', lg: '40%' } }}>
-            <OngoingClass />
+            {loading ? <WidgetSkeleton height={260} /> : <OngoingClass />}
           </Box>
         </Box>
 
@@ -69,12 +77,12 @@ const DashboardPage = () => {
         }}>
           {/* Information Slider - 30% width on desktop */}
           <Box sx={{ width: { xs: '100%', lg: '40%' } }}>
-            <InformationSlider />
+            {loading ? <WidgetSkeleton height={180} /> : <InformationSlider />}
           </Box>
 
           {/* Pending Assessments - 70% width on desktop */}
           <Box sx={{ width: { xs: '100%', lg: '60%' } }}>
-            <PendingAssessments />
+            {loading ? <WidgetSkeleton height={180} /> : <PendingAssessments />}
           </Box>
         </Box>
       </Container>
